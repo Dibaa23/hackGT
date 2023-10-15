fa76c4c46b013f335f677447fb64034124e8e2b5
Calvin Truong <ctruong34@gatech.edu>
2023-10-15 07:55:09 -0400

    final

--- assets/py/main.py
+++ assets/py/main.py
@@ -5,45 +5,52 @@ from flask_cors import CORS
 import utils
 import Goal
 
+
+
+
 app = Flask(__name__)
 savings = 1000
-purchases = [("Dining", 25.00),            # Breakfast at Local Café,
-    ("Dining", 40.00),            # Friday Night Dinner at Italian Restaurant,
-    ("Gas/Automotive", 35.00),    # Weekly Gas Fill-Up,
-    ("Grocery", 100.00),          # Weekly Grocery Shopping,
-    ("Healthcare", 50.00),        # Prescription Medication,
-    ("Lodging", 150.00),          # Weekend Getaway Hotel Stay,
-    ("Merchandise", 80.00),       # Online Clothes Shopping,
-    ("Other", 30.00),             # Monthly Streaming Subscription,
-    ("Other Services", 100.00),   # Haircut and Salon Service,
-    ("Payment", 1200.00),         # Monthly Rent Payment,
+income = 9999
+purchases = [
+    ("Dining", 25.00, "Breakfast at Local Café"),
+    ("Dining", 40.00, "Friday Night Dinner at Italian Restaurant"),
+    ("Gas/Automotive", 35.00, "Weekly Gas Fill-Up"),
+    ("Grocery", 100.00, "Weekly Grocery Shopping"),
+    ("Healthcare", 50.00, "Prescription Medication"),
+    ("Lodging", 150.00, "Weekend Getaway Hotel Stay"),
+    ("Merchandise", 80.00, "Online Clothes Shopping"),
+    ("Other", 30.00, "Monthly Streaming Subscription"),
+    ("Other Services", 100.00, "Haircut and Salon Service"),
+    ("Payment", 1200.00, "Monthly Rent Payment"),
 
-    ("Dining", 12.00),            # Coffee and Sandwich at Local Café,
-    ("Dining", 50.00),            # Saturday Night Dinner at Sushi Restaurant,
-    ("Gas/Automotive", 35.00),    # Weekly Gas Fill-Up,
-    ("Grocery", 95.00),           # Weekly Grocery Shopping,
-    ("Healthcare", 25.00),        # Over-the-counter Medication,
-    ("Merchandise", 60.00),       # Shoes from Department Store,
-    ("Other", 10.00),             # Monthly Magazine Subscription,
-    ("Other Services", 60.00),    # House Cleaning Service,
-    ("Payment", 100.00),          # Utility Bill,
+    ("Dining", 12.00, "Coffee and Sandwich at Local Café"),
+    ("Dining", 50.00, "Saturday Night Dinner at Sushi Restaurant"),
+    ("Gas/Automotive", 35.00, "Weekly Gas Fill-Up"),
+    ("Grocery", 95.00, "Weekly Grocery Shopping"),
+    ("Healthcare", 25.00, "Over-the-counter Medication"),
+    ("Merchandise", 60.00, "Shoes from Department Store"),
+    ("Other", 10.00, "Monthly Magazine Subscription"),
+    ("Other Services", 60.00, "House Cleaning Service"),
+    ("Payment", 100.00, "Utility Bill"),
 
-    ("Dining", 10.00),            # Coffee and Muffin at Local Café,
-    ("Gas/Automotive", 40.00),    # Weekly Gas Fill-Up,
-    ("Grocery", 110.00),          # Weekly Grocery Shopping,
-    ("Lodging", 180.00),          # Hotel for Friend's Wedding,
-    ("Merchandise", 20.00),       # Book from Local Bookstore,
-    ("Other Services", 50.00),    # Manicure and Pedicure,
-    ("Payment", 50.00),           # Cell Phone Bill,
+    ("Dining", 10.00, "Coffee and Muffin at Local Café"),
+    ("Gas/Automotive", 40.00, "Weekly Gas Fill-Up"),
+    ("Grocery", 110.00, "Weekly Grocery Shopping"),
+    ("Lodging", 180.00, "Hotel for Friend's Wedding"),
+    ("Merchandise", 20.00, "Book from Local Bookstore"),
+    ("Other Services", 50.00, "Manicure and Pedicure"),
+    ("Payment", 50.00, "Cell Phone Bill"),
 
-    ("Dining", 15.00),            # Salad and Juice at Health Food Restaurant,
-    ("Gas/Automotive", 38.00),    # Weekly Gas Fill-Up,
-    ("Grocery", 85.00),           # Weekly Grocery Shopping,
-    ("Merchandise", 40.00),       # Kitchen Appliance from Online Store,
-    ("Other", 15.00),             # Monthly Music Streaming Subscription,
-    ("Payment", 60.00)           # Internet Bill
+    ("Dining", 15.00, "Salad and Juice at Health Food Restaurant"),
+    ("Gas/Automotive", 38.00, "Weekly Gas Fill-Up"),
+    ("Grocery", 85.00, "Weekly Grocery Shopping"),
+    ("Merchandise", 40.00, "Kitchen Appliance from Online Store"),
+    ("Other", 15.00, "Monthly Music Streaming Subscription"),
+    ("Payment", 60.00, "Internet Bill")
 ]
+
 investments = []
+purchase_dict = {}
 goals = Goal.Goals(savings)
 
 
@@ -51,6 +58,16 @@ goals = Goal.Goals(savings)
 def index():
     return render_template("index.html")
 
+@app.route('/report', methods=['GET'])
+def report():
+    type_list = []
+    value_list = []
+    description_list = []
+    for i in range(0, len(purchases)):
+        type_list.append(purchases[i][0])
+        value_list.append(purchases[i][1])
+        description_list.append(purchases[i][2])
+    return jsonify({"type_list": type_list,"value_list": value_list,"description_list": description_list})
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -67,6 +84,10 @@ def update_savings():
 @app.route('/savings', methods=['get'])
 def get_savings():
     return jsonify({"savings": savings})
+
+@app.route('/income', methods=['GET'])
+def get_income():
+    return jsonify({"income": income})
 
 @app.route('/goal', methods=['contribute'])
 def contribute():
@@ -102,7 +123,7 @@ def change_total():
     return jsonify({"message": goals.change_total(goal, total)})
 
 
-@app.route('/gpt', methods=['purchase'])
+@app.route('/gpt', methods=['GET'])
 def gpt_rec():
     return jsonify({"message": utils.gpt_rec(purchases)})
 
@@ -112,10 +133,10 @@ def gpt_inv():
     return jsonify({"message": utils.gpt_rec(investments)})
 
 
-@app.route('/score', methods=['calculate'])
+@app.route('/score', methods=['GET'])
 def calculate_score():
-    needs, wants, situational = utils.categorize_transaction(purchases)
-    return jsonify({"score": utils.calculate_score(needs, situational, wants)})
+    purchase_dict = utils.categorize_transaction(purchases)
+    return jsonify({"score": utils.calculate_score(purchase_dict,income)})
 
 
 @app.route('/expenses', methods=['analyze'])
@@ -125,9 +146,17 @@ def analyze_expenses():
 
 @app.route('/transactions', methods=['GET'])
 def categorize_transactions():
-    needs, wants, situational = utils.categorize_transaction(purchases)
-    return jsonify({"needs": needs, "wants": wants, "situational": situational})
+    purchase_dict = utils.categorize_transaction(purchases)
+    nsw_list = utils.three_expense_categories(purchase_dict)
+    nsw_list = [x / sum(nsw_list) for x in nsw_list]
+    return jsonify({"needs": nsw_list[0], "wants": nsw_list[2], "situational": nsw_list[1]})
 
+
+@app.route('/EA', methods=['GET'])
+def expected_actual_spending():
+    purchase_dict = utils.categorize_transaction(purchases)
+    l = utils.expected_actual_spending(purchase_dict, int(income / 12), income)
+    return jsonify({"e_needs": l[0], "e_wants": l[2], "e_situational": l[1], "needs": l[3], "wants": l[5], "situational": l[4], "income": int(income / 12)})
 
 @app.route('/reminder', methods=['remind'])
 def remind():
--- assets/py/utils.py
+++ assets/py/utils.py
@@ -1,4 +1,203 @@
 import openai
+import math
+
+openai.api_key = "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"
+
+# Asks purchases question to ChatGPT for advice - DONE
+def gpt_rec(purchases):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": \
+             "Could you help analyze these purchases and give me some recommendations? Type the string in a format best suitable for textContent HTML and make it 50 words and in one paragraph."},
+            {"role": "user", "content": str(purchases)}
+        ]
+    )
+    return response['choices'][0]['message']['content']
+
+# Asks investments question to ChatGPT for advice - DONE
+def gpt_inv(investments):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": \
+             "Could you help analyze these investments and give me some recommendations?"},
+            {"role": "user", "content": str(investments)}
+        ]
+    )
+    return response['choices'][0]['message']['content']
+
+# Minimum calculator - DONE
+def minimum(num1, num2):
+    if num1 <= num2:
+        return num1
+    else:
+        return num2
+
+# Income int function - DONE
+def income_int_funct(net_income):
+    income_int = 0
+    if net_income < 20000:
+        income_int = 0
+    elif net_income < 30000:
+        income_int = 1
+    elif net_income < 40000:
+        income_int = 2
+    elif net_income < 50000:
+        income_int = 3
+    elif net_income < 60000:
+        income_int = 4
+    elif net_income < 70000:
+        income_int = 5
+    elif net_income < 80000:
+        income_int = 6
+    elif net_income < 90000:
+        income_int = 7
+    elif net_income < 100000:
+        income_int = 8
+    else:
+        income_int = 9
+
+    return income_int
+
+# Returns target percents for situational, wants + situational weight factor for score equation - DONE
+def need_sit_want_percents_specific(net_income):
+    income_int = income_int_funct(net_income)
+    finance_list = [[0.15, 0.1], [0.15, 0.12], [0.15, 0.12], [0.18, 0.15], [0.18, 0.15], [0.18, 0.17], [0.18, 0.17], [0.2, 0.18], [0.2, 0.18], [0.2, 0.2]]
+    constant_list = [0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75]
+
+    return [finance_list[income_int], constant_list[income_int]]
+
+# Calculates score - DONE
+def calculate_score(purchase_dict, net_income):
+    list_of_finance_constants = need_sit_want_percents_specific(net_income)
+    constant = list_of_finance_constants[1]
+    target_situational = list_of_finance_constants[0][0]
+    target_wants = list_of_finance_constants[0][1]
+
+    nsw_list = three_expense_categories(purchase_dict)
+    nsw_list = [x / sum(nsw_list) for x in nsw_list]
+    situational = nsw_list[1]
+    wants = nsw_list[2]
+
+    score = minimum((1 - constant * (situational - target_situational) - (wants - target_wants)) * 100, 100)
+    return score
+
+# Calculates percentages of needs, wants, & situational based on purchases types - DONE
+def categorize_transaction(list_of_purchases, purchase_dict = {"Dining": 0, "Gas/Automotive": 0, "Grocery": 0, \
+                                                               "Healthcare": 0, "Lodging": 0, "Merchandise": 0, "Other": 0, \
+                                                                "Other Services": 0, "Payment": 0}):
+    for purchase in list_of_purchases:
+        for item in purchase_dict.keys():
+            if purchase[0] == item:
+                purchase_dict[item] += purchase[1]
+
+    return purchase_dict
+
+# Counts of main 3, and percentages of main 3 as percent of total expenses - DONE
+def three_expense_categories(purchase_dict):
+    needs = purchase_dict["Grocery"] + purchase_dict["Healthcare"] + purchase_dict["Payment"]
+    situational = purchase_dict["Dining"] + purchase_dict["Gas/Automotive"] + 0.25 * purchase_dict["Other"]
+    wants = 0.75 * purchase_dict["Other"] + purchase_dict["Lodging"] + purchase_dict["Merchandise"] + purchase_dict["Other Services"]
+
+    return [needs, situational, wants]
+
+# Counts of all categories as percent of total expenses - DONE
+def all_expense_categories(purchase_dict):
+    grocery = purchase_dict["Grocery"]
+    health = purchase_dict["Healthcare"]
+    payment = purchase_dict["Payment"]
+    dining = purchase_dict["Dining"]
+    gas_auto = purchase_dict["Gas/Automotive"]
+    other = purchase_dict["Other"]
+    lodging = purchase_dict["Lodging"]
+    merch = purchase_dict["Merchandise"]
+    other_servs = purchase_dict["Other Services"]
+
+    return [grocery, health, payment, dining, gas_auto, other, lodging, merch, other_servs]
+
+# Percentages of sub-categories as percent of needs - DONE
+def nested_needs_percentages(purchase_dict):
+    all_catg_list = all_expense_categories(purchase_dict)[0:3]
+    needs_percent_list = [x / sum(all_catg_list) for x in all_catg_list]
+
+    return needs_percent_list
+
+# Percentages of sub-categories as percent of situational - DONE
+def nested_situational_percentages(purchase_dict):
+    all_catg_list = all_expense_categories(purchase_dict)[3:6]
+    all_catg_list = [all_catg_list[0], all_catg_list[1], 0.25 * all_catg_list[2]]
+    situational_percent_list = [x / sum(all_catg_list) for x in all_catg_list]
+
+    return situational_percent_list
+
+# Percentages of sub-categories as percent of wants - DONE
+def nested_wants_percentages(purchase_dict):
+    all_catg_list = all_expense_categories(purchase_dict)[5:]
+    all_catg_list = [0.75 * all_catg_list[0], all_catg_list[1], all_catg_list[2], all_catg_list[3]]
+    wants_percent_list = [x / sum(all_catg_list) for x in all_catg_list]
+
+    return wants_percent_list
+
+# Difference in spending (actual v. expected) - DONE
+def expected_actual_spending(purchase_dict, month_income, net_income):
+    nsw_list = three_expense_categories(purchase_dict)
+    nsw_value_income_list = [x / sum(nsw_list) * month_income for x in nsw_list]
+
+    list_of_financial_constraints = need_sit_want_percents_specific(net_income)
+    situational_percent = list_of_financial_constraints[0][0]
+    wants_percent = list_of_financial_constraints[0][1]
+    needs_percent = 1 - situational_percent - wants_percent
+
+    return [nsw_value_income_list[0], nsw_value_income_list[1], nsw_value_income_list[2], \
+            needs_percent * month_income, situational_percent * month_income, wants_percent * month_income]
+
+# Analyzes expenses - DONE
+def analyze_expenses(purchase_dict, net_income):
+    nsw_list = three_expense_categories(purchase_dict)
+    nsw_percent_list = [x / sum(nsw_list) for x in nsw_list]
+
+    list_of_finance_constants = need_sit_want_percents_specific(net_income)
+
+    exceeded_bools_list = [False, False]
+    diff_in_sit_wants_list = [0, 0]
+
+    for i in range(1, len(nsw_percent_list)):
+        if nsw_percent_list[i] > list_of_finance_constants[0][i - 1]:
+            exceeded_bools_list[i - 1] = True
+            diff_in_sit_wants_list[i - 1] = nsw_percent_list[i] - list_of_finance_constants[0][i - 1]
+
+    situational_diff = math.ceil(diff_in_sit_wants_list[0] * 100)
+    wants_diff = math.ceil(diff_in_sit_wants_list[1] * 100)
+
+    worst = 0
+    indicator = ""
+
+    if exceeded_bools_list[0] and situational_diff > wants_diff:
+        worst = situational_diff
+        indicator = "Situational"
+    elif exceeded_bools_list[1] and wants_diff > situational_diff:
+        worst = wants_diff
+        indicator = "Wants"
+    
+    if indicator == "Situational":
+        return f'Your expenses are higher than expected by {worst} percent. \
+            Please try and reduce your areas of spending in Dining, Gas/Automotive, and Other.'
+
+    if indicator == "Wants":
+        return f'Your expenses are higher than expected by {worst} percent. \
+            Please try and reduce your areas of spending in Lodging, Merchandice, Other, and Other Services.'
+
+
+# How often to send opted-in reminder to pay bills - DONE
+def need_reminder(time, week_factor = 1):
+    if time > 60 * 60 * 24 * 7 * week_factor:
+        return True
+    return False
+
+
+'''
+import openai
 
 openai.api_key = "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"
 
@@ -100,3 +299,4 @@ def need_reminder(time):
     if time > 1000000:
         return True
     return False
+'''
